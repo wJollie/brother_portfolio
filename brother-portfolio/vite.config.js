c80a31cf45abef06b54ebabc0706c090d832f882
wjollie <william.jollie23@gmail.com>
2025-08-24 13:28:12 -0400

    netlify workaround

--- brother-portfolio/vite.config.js
+++ brother-portfolio/vite.config.js
@@ -1,7 +1,8 @@
-// vite.config.js
+// brother-portfolio/vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // <-- not '/brother_portfolio/' (that’s only for GitHub Pages)
 });
